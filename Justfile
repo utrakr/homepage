4595a0d7f65702f76f696883a9b072bd123699b1
Cameron Davison <cdavison@digitalpharmacist.com>
2020-05-30 21:05:39 -0500

    fmt prettier

--- Justfile
+++ Justfile
@@ -1,2 +1,5 @@
+fmt:
+    prettier --write --tab-width 4 --print-width 120 static/js/
+
 develop:
     zola --config config.local.toml serve
--- static/js/base.js
+++ static/js/base.js
@@ -9,36 +9,40 @@ async function fillInClipboard(inputElm, callback) {
 }
 
 function setup() {
-    const msgElm = document.getElementById('info-message');
-    const formElm = document.getElementById('long-url-form');
+    const msgElm = document.getElementById("info-message");
+    const formElm = document.getElementById("long-url-form");
     if (!msgElm || !formElm) {
         return;
     }
 
     // paste
-    const pasteElm = document.getElementById('long-url-paste');
-    const inputElm = document.getElementById('long-url-input');
+    const pasteElm = document.getElementById("long-url-paste");
+    const inputElm = document.getElementById("long-url-input");
     if (inputElm && pasteElm) {
         pasteElm.onclick = async function (event) {
             event.preventDefault();
-            await fillInClipboard(inputElm, () => submitFormData(msgElm, formElm))
-        }
+            await fillInClipboard(inputElm, () => submitFormData(msgElm, formElm));
+        };
     }
 
     // form
-    formElm.addEventListener( "submit", function ( event ) {
+    formElm.addEventListener("submit", function (event) {
         event.preventDefault();
         submitFormData(msgElm, formElm);
-    } );
+    });
 }
 
 function submitFormData(msgElm, formElm) {
-    sendData(formElm, (data) => {
-        console.info('data', data);
-        msgElm.innerHTML = `<span class="o-message-success">Your new micro url is <a href="${data['micro_url']}">${data['micro_url']}</a></span>`
-    }, (error) => {
-        console.error('error', error);
-    });
+    sendData(
+        formElm,
+        (data) => {
+            console.info("data", data);
+            msgElm.innerHTML = `<span class="o-message-success">Your new micro url is <a href="${data["micro_url"]}">${data["micro_url"]}</a></span>`;
+        },
+        (error) => {
+            console.error("error", error);
+        }
+    );
 }
 
 function sendData(form, onSuccess, onError) {
@@ -54,31 +58,31 @@ function sendData(form, onSuccess, onError) {
             if (onSuccess) {
                 const responseText = tgt.responseText;
                 const data = JSON.parse(responseText);
-                onSuccess(data)
+                onSuccess(data);
             }
         } else {
-            onError({'message': 'Bad Response', 'status': status})
+            onError({ message: "Bad Response", status: status });
         }
     });
 
     if (onError) {
         XHR.addEventListener("error", function (event) {
-            console.error('target', event.target);
-            onError({'message': 'No Response'});
+            console.error("target", event.target);
+            onError({ message: "No Response" });
         });
     }
 
     // create and send data
     const data = {};
     const FD = new FormData(form);
-    for (const [k,v] of FD) {
+    for (const [k, v] of FD) {
         data[k] = v;
     }
     XHR.open(formMethod, formAction);
-    XHR.setRequestHeader( 'Content-Type', 'application/json' );
+    XHR.setRequestHeader("Content-Type", "application/json");
     XHR.send(JSON.stringify(data));
 }
 
-window.addEventListener( "load", function () {
+window.addEventListener("load", function () {
     setup();
 });
